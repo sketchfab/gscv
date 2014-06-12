eb685f06921d3a81e3272313b5b3be7a60697377
Fiahil <fiahil@gmail.com>
2014-06-11 23:05:17 -0700

    remove admin and auth

--- gscv/urls.py
+++ gscv/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
     url(r'^api/', include('cards.urls')),
     url(r'^$', include('editor.urls')),
-    url(r'^admin/', include(admin.site.urls)),
 )
