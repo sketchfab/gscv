7d4528bfcf640a4308f14590c43fa15799f09f86
Shirese <chaueur@gmail.com>
2015-08-05 17:11:25 +0200

    - Added Text group
    - Added police widgets :
    	- Font
    	- FontSize
    	- FontColor

--- sources/app.js
+++ sources/app.js
@@ -12,9 +12,12 @@ define( [
     var Card = Backbone.Model.extend( {
 
         defaults : {
-            radius : 10,
-            name   : "Maël Nison",
-            job    : "FrontEnd Developer"
+            radius   : 10,
+            name     : "Maël Nison",
+            job      : "FrontEnd Developer",
+            font     : "Courier New",
+            fontSize : 11,
+            fontColor : "rgb( 0, 0, 0)"
         }
 
     } );
@@ -24,12 +27,18 @@ define( [
         initialize : function ( ) {
             this.model.on( 'change:name', this.onNameChange, this );
             this.model.on( 'change:job', this.onJobChange, this );
+            this.model.on( 'change:font', this.onFontChange, this );
+            this.model.on( 'change:fontSize', this.onFontSizeChange, this );
+            this.model.on( 'change:fontColor', this.onFontColorChange, this );
             this.model.on( 'change:radius', this.onRadiusChange, this );        
         },
 
         render : function ( ) {
             this.onNameChange( );
             this.onJobChange( );
+            this.onFontChange( );
+            this.onFontSizeChange( );
+            this.onFontColorChange( );
             this.onRadiusChange( );  
         },
 
@@ -39,6 +48,24 @@ define( [
 
         onJobChange : function ( ) {
             this.$el.children( 'div.job' ).text( this.model.get( 'job' ) );
+        },
+
+        onFontChange : function ( ) {
+            this.$el.css( 'font-family', this.model.get( 'font') );
+        },
+
+        onFontSizeChange : function ( ) {
+            this.$el.css( 'font-size', this.model.get( 'fontSize' ) );
+        },
+
+        onFontColorChange : function ( ) {
+            // Color.js is probably badly understood...
+            var r = Math.round ( this.model.get('fontColor')["r"] * 255 );
+            var g = Math.round ( this.model.get('fontColor')["g"] * 255 );
+            var b = Math.round ( this.model.get('fontColor')["b"] * 255 );
+            var rgb = "rgb(" + r + "," + g + "," + b + ")";
+            this.$el.children( 'div.name' ).css( 'color', rgb );
+            this.$el.children( 'div.job' ).css( 'color', rgb );
         },
 
         onRadiusChange : function ( ) {
@@ -60,19 +87,42 @@ define( [
         label : 'Card Appearance'
     } );
 
-    appearance.createWidget( 'Name', 'Text', {
-        model : card,
-        name  : 'name'
-    } );
-
-    appearance.createWidget( 'Job', 'Text', {
-        model : card,
-        name  : 'job'
-    } );
-
     appearance.createWidget( 'Border radius', 'NumberedSlider', {
         model : card,
         name  : 'radius'
     } );
+
+    // --- --- --- --- --- --- --- --- ---
+
+    var textAppearance = editor.createWidget( 'Group', {
+        label : 'Text Appearance'
+    } );
+
+    textAppearance.createWidget( 'Name', 'Text', {
+        model : card,
+        name  : 'name'
+    } );
+
+    textAppearance.createWidget( 'Job', 'Text', {
+        model : card,
+        name  : 'job'
+    } );
+
+    textAppearance.createWidget( 'Font', 'Select', {
+        model : card,
+        options : ['Arial', 'Courier New', 'Helvetica', 'Impact', 'Verdana'],
+        name  : 'font'
+    } );
+
+    textAppearance.createWidget( 'Font Size', 'NumberedSlider', {
+        model : card,
+        name  : 'fontSize'
+    } );
+
+    textAppearance.createWidget( 'Font Color', 'Color', {
+        model : card,
+        name  : 'fontColor'
+    } );
+    
 
 } );
