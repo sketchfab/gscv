222f707e8bddfb642561fc4550e16e2868f44581
Clément Delaunay <clem.delaunay@gmail.com>
2016-03-20 20:46:45 +0100

    [FEATURE] Font family, Contact block, Contact font-size, Text color, Background color, Background image

--- sources/app.js
+++ sources/app.js
@@ -12,6 +12,8 @@ define( [
     var Card = Backbone.Model.extend( {
 
         defaults : {
+            initColorsCard: false,
+            initColorsText: false,
             radius : 10,
             width : 550,
             height : 300,
@@ -20,15 +22,36 @@ define( [
             pictureRadius:5,
             pictureScale:70,
             pictureOffset: 60,
-            name:"",
-            job: "",
+            name:'Clément Delaunay',
+            job: 'Développeur Web',
+            email : 'clem.delaunay@gmail.com',
+            website : 'clement.io',
+            phone : '+336 87 25 09 44',
             nameFontSize:16,
-            jobFontSize:16
+            jobFontSize:16,
+            contactFontSize: 16,
+            pictureBackgroundUrl : '',
+            backgroundColor: {rgb: {r: 44, g: 44, b: 44}, hex:'#2C2C2C'},
+            textColor: {rgb: {r: 255, g: 255, b: 255}, hex:'#FFFFFF'},
+            fontFamily: 'Lato'
         }
 
     } );
 
-    var View = Backbone.View.extend( {
+    var PanelView = Backbone.View.extend( {
+        events: {
+            'click .color-widget .colorCard' : 'initColorsCard',
+            'click .color-widget .colorText' : 'initColorsText'
+        },
+        initColorsCard: function(){
+            if(!this.model.get( 'initColorsCard' )) this.model.set( 'initColorsCard', true);
+        },
+        initColorsText: function(){
+            if(!this.model.get( 'initColorsText' )) this.model.set( 'initColorsText', true);
+        }
+    })
+
+    var CardView = Backbone.View.extend( {
 
         initialize : function ( ) {
             this.model.on( 'change:radius', this.onRadiusChange, this );
@@ -38,21 +61,48 @@ define( [
             this.model.on( 'change:pictureScale', this.onPictureScaleChange, this );
             this.model.on( 'change:name', this.onNameChange, this );
             this.model.on( 'change:job', this.onJobChange, this );
+            this.model.on( 'change:email', this.onEmailChange, this );
+            this.model.on( 'change:website', this.onWebsiteChange, this );
+            this.model.on( 'change:phone', this.onPhoneChange, this );
             this.model.on( 'change:nameFontSize', this.onNameFontSizeChange, this );
             this.model.on( 'change:jobFontSize', this.onJobFontSizeChange, this );
-            this.model.on( 'uploadSelectEvent', this.onPictureUrlChange, this );
+            this.model.on( 'change:contactFontSize', this.onContactFontSizeChange, this );
+            this.model.on( 'profilPictureUpload', this.onProfilePictureUrlChange, this );
+            this.model.on( 'backgroundPictureUpload', this.onBackgroundPictureUrlChange, this );
+            this.model.on( 'change:initColorsCard', this.onBackgroundColorChange, this );
+            this.model.on( 'change:initColorsText', this.onTextColorChange, this );
+            this.model.on( 'change:backgroundColor', this.onBackgroundColorChange, this );
+            this.model.on( 'change:textColor', this.onTextColorChange, this );
+            this.model.on( 'change:fontFamily', this.onFontFamilyChange, this );
             this.render();
         },
 
         render : function ( ) {
             this.onRadiusChange();
-            this.onPictureUrlChange();
+            this.onProfilePictureUrlChange();
             this.onPictureRadiusChange();
             this.onPictureScaleChange();
+
+            this.onNameChange();
+            this.onJobChange();
+            this.onEmailChange();
+            this.onWebsiteChange();
+            this.onPhoneChange();
         },
 
         onRadiusChange : function ( ){
             this.$el.css( 'border-radius', this.model.get( 'radius' ) );
+        }, 
+        onBackgroundColorChange: function ( ){
+            var newColor = this.model.get( 'backgroundColor' );
+            this.$el.css( 'background-image', '' );
+            if(this.model.get( 'initColorsCard' )) this.$el.css( 'background-color', newColor.hex );
+            else this.$el.css( 'background-color', '#2C2C2C');
+        }, 
+        onTextColorChange: function ( ){
+            var newColor = this.model.get( 'textColor' );
+            if(this.model.get( 'initColorsText' )) this.$el.css( 'color', newColor.hex );
+            else this.$el.css( 'color', '#FFFFFF');
         }, 
         onWidthChange : function ( ){
             this.$el.css( 'width', this.model.get( 'width' ) );
@@ -62,9 +112,13 @@ define( [
             this.$el.css( 'height', this.model.get( 'height' ) );
             this.onPictureScaleChange();
         },
-        onPictureUrlChange : function ( file ){
+        onProfilePictureUrlChange : function ( file ){
             if( file != undefined ) this.model.set( 'pictureUrl', URL.createObjectURL( file ));
             this.$('.picture').css( 'background-image', 'url(\'' + this.model.get( 'pictureUrl' ) + '\')' );
+        },
+        onBackgroundPictureUrlChange : function ( file ){
+           if( file != undefined ) this.model.set( 'pictureBackgroundUrl', URL.createObjectURL( file ));
+           this.$el.css( 'background-image', 'url(\'' + this.model.get( 'pictureBackgroundUrl' ) + '\')' );
         },
         onPictureRadiusChange : function ( ){
             this.$('.picture').css( 'border-radius', this.model.get( 'pictureRadius' ) );
@@ -75,11 +129,26 @@ define( [
         onJobChange : function ( ){
             this.$('.job').text( this.model.get( 'job' ) );
         },
-        onNameFontSizeChange : function ( element ){
+        onEmailChange : function ( ){
+            this.$('.email').text( this.model.get( 'email' ) );
+        },
+        onWebsiteChange : function ( ){
+            this.$('.website').text( this.model.get( 'website' ) );
+        },
+        onPhoneChange : function ( ){
+            this.$('.phone').text( this.model.get( 'phone' ) );
+        },
+        onNameFontSizeChange : function ( ){
             this.$('.informations div:first-child()').css( 'font-size', this.model.get( 'nameFontSize' ) );
         },
-        onJobFontSizeChange : function ( element ){
+        onJobFontSizeChange : function ( ){
             this.$('.informations div:last-child()').css( 'font-size', this.model.get( 'jobFontSize' ) );
+        },
+        onContactFontSizeChange : function ( ){
+            this.$('.contact').css( 'font-size', this.model.get( 'contactFontSize' ) );
+        },
+        onFontFamilyChange : function ( ){
+           this.$el.css( 'font-family', this.model.get( 'fontFamily' ) );
         },
         onPictureScaleChange: function ( ){
             var newSize = parseInt(this.model.get( 'pictureSize' ))+this.model.get( 'pictureScale' ),
@@ -108,7 +177,9 @@ define( [
     // --- --- --- --- --- --- --- --- ---
 
     var card = new Card( );
-    var cardView = new View( { model : card, el : $( '.card' ) } );
+    var cardView = new CardView( { model : card, el : $( '.card' ) } );
+
+    var panelView = new PanelView( { model: card, el : $('.editor') } );
 
     cardView.render( );
 
@@ -140,12 +211,14 @@ define( [
     cardAppearance.createWidget( 'Upload a background picture', 'FilePicker', {
         model : card,
         name  : 'pictureBackgroundUrl',
-        text: 'Choose picture'
+        text: 'Choose picture',
+        selectEvent: 'backgroundPictureUpload'
     } );
 
     cardAppearance.createWidget( 'Or pick a background color', 'Color', {
         model : card,
-        name  : 'backgroundColor'
+        name  : 'backgroundColor',
+        cssClass : 'colorCard'
     } );
 
     var pictureAppearance = editor.createWidget( 'Group', {
@@ -155,7 +228,8 @@ define( [
     pictureAppearance.createWidget( 'Upload a picture', 'FilePicker', {
         model : card,
         name  : 'pictureUrl',
-        text: 'Choose picture'
+        text: 'Choose picture',
+        selectEvent: 'profilPictureUpload'
     } );
 
     pictureAppearance.createWidget( 'Picture size', 'NumberedSlider', {
@@ -232,16 +306,15 @@ define( [
 
     textAppearance.createWidget( 'Font Family', 'Select', {
         model : card,
-        name  : 'jobFontSize',
+        name  : 'fontFamily',
         placeholder : 'Font Family',
         options : ['Lato', 'Montserrat', 'Raleway', 'Lora']
     } );
 
     textAppearance.createWidget( 'Text Color', 'Color', {
         model : card,
-        name  : 'textColor'
+        name  : 'textColor',
+        cssClass : 'colorText'
     } );
-
-
 
 } );
