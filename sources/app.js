2b5370d543143c12bccadff223cc19656dbe42f9
Pierre Besson <pierre.besson7@gmail.com>
2016-06-25 13:09:13 +0200

    [app] Use text widget for name and job.

--- sources/app.js
+++ sources/app.js
@@ -4,17 +4,26 @@ define( [
 
     'vendors/Backbone',
     'vendors/JQuery',
+    'editor',
+    'customWidget/Text'
 
-    'editor'
+], function ( Backbone, $, editor, TextWidget ) {
 
-], function ( Backbone, $, editor ) {
+    // Just to have non empty defaults
+    var PIERRE_DEFAULTS = {
+      radius: 7,
+      name: 'Pierre Besson',
+      job: 'Bientôt chez sketchfab'
+    };
 
     var Card = Backbone.Model.extend( {
 
-        defaults : {
+        defaults : _.extend({
             radius : 10,
-            lol: 2
-        }
+            // Identity defaults
+            name: '',
+            job: ''
+        }, PIERRE_DEFAULTS)
 
     } );
 
@@ -22,15 +31,23 @@ define( [
 
         initialize : function ( ) {
             this.model.on( 'change:radius', this.onRadiusChange, this );
-            this.model.on( 'change:lol', this.onLolChange, this );
+            this.model.on( 'change:name', this.render, this );
+            this.model.on( 'change:job', this.render, this );
+
         },
 
         render : function ( ) {
+            // I'd rather use templating on this view because it gets informations
+            // from all the wiget
+            // An maintaining its rendering from sevral model property is mode readable with a render.
+            // Even if this is maybe not the most optimal way.
+            // I use ES2015 string interpolation to build the template, it could be a function.
+            this.$el.html(`
+              <div class="name">${this.model.get('name')}</div>
+              <div class="job">${this.model.get('job')}</div>
+              `);
+
             this.onRadiusChange( );
-        },
-        onLolChange : function onLolChange( ) {
-            const name = this.$el.find('div.name');
-            name.text( name.text()+ ' ' + this.model.get('lol'));
         },
         onRadiusChange : function ( ) {
             this.$el.css( 'border-radius', this.model.get( 'radius' ) );
@@ -46,7 +63,7 @@ define( [
     view.render( );
 
     // --- --- --- --- --- --- --- --- ---
-
+    // Appearance
     var appearance = editor.createWidget( 'Group', {
         label : 'Card Appearance'
     } );
@@ -55,9 +72,17 @@ define( [
         model : card,
         name  : 'radius'
     } );
-    appearance.createWidget( 'Border radius', 'Angle', {
+    // Identity
+    var identity = editor.createWidget('Group', {
+      label: 'Identity'
+    })
+    identity.createWidget( 'Nom', TextWidget , {
         model : card,
-        name  : 'lol'
+        name  : 'name'
+    } );
+    identity.createWidget( 'Job', TextWidget , {
+        model : card,
+        name  : 'job'
     } );
 
 } );
