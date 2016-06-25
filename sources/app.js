03c41510927636271fe08815f093b14b19b19268
Pierre Besson <pierre.besson7@gmail.com>
2016-06-25 13:02:08 +0200

    [app] Test wiget api

--- sources/app.js
+++ sources/app.js
@@ -12,7 +12,8 @@ define( [
     var Card = Backbone.Model.extend( {
 
         defaults : {
-            radius : 10
+            radius : 10,
+            lol: 2
         }
 
     } );
@@ -21,12 +22,16 @@ define( [
 
         initialize : function ( ) {
             this.model.on( 'change:radius', this.onRadiusChange, this );
+            this.model.on( 'change:lol', this.onLolChange, this );
         },
 
         render : function ( ) {
             this.onRadiusChange( );
         },
-
+        onLolChange : function onLolChange( ) {
+            const name = this.$el.find('div.name');
+            name.text( name.text()+ ' ' + this.model.get('lol'));
+        },
         onRadiusChange : function ( ) {
             this.$el.css( 'border-radius', this.model.get( 'radius' ) );
         }
@@ -49,6 +54,10 @@ define( [
     appearance.createWidget( 'Border radius', 'NumberedSlider', {
         model : card,
         name  : 'radius'
+    } );
+    appearance.createWidget( 'Border radius', 'Angle', {
+        model : card,
+        name  : 'lol'
     } );
 
 } );
