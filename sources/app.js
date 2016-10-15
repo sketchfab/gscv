5bcf2299e8741250ef7589c9f685d1290698f4da
bcrochez <bastien.crochez@gmail.com>
2016-10-15 12:15:38 +0200

    Add background & font color

--- sources/app.js
+++ sources/app.js
@@ -13,7 +13,9 @@ define( [
 
         defaults : {
             radius : 10,
+            cardBackgroundColor : { r : 0.17255, g : 0.17255, b : 0.17255 },
             fontsize : 24,
+            fontColor : { r : 1, g : 1, b : 1 },
             name : 'My Name',
             job : 'My job',
         }
@@ -24,14 +26,18 @@ define( [
 
         initialize : function ( ) {
             this.model.on( 'change:radius', this.onRadiusChange, this );
+            this.model.on( 'change:cardBackgroundColor', this.onCardBackgroundColorChange, this );
             this.model.on( 'change:fontsize', this.onFontSizeChange, this );
+            this.model.on( 'change:fontColor', this.onFontColorChange, this );
             this.model.on( 'change:name', this.onNameChange, this );
             this.model.on( 'change:job', this.onJobChange, this );
         },
 
         render : function ( ) {
             this.onRadiusChange( );
+            this.onCardBackgroundColorChange( );
             this.onFontSizeChange( );
+            this.onFontColorChange();
             this.onNameChange( );
             this.onJobChange( );
         },
@@ -40,8 +46,16 @@ define( [
             this.$el.css( 'border-radius', this.model.get( 'radius' ) );
         },
 
+        onCardBackgroundColorChange : function ( ) {
+            this.$el.css( 'background-color', this.rgbToHex( this.model.get( 'cardBackgroundColor' ) ) );
+        },
+
         onFontSizeChange : function ( ) {
             this.$el.css( 'font-size', this.model.get( 'fontsize' ) );
+        },
+
+        onFontColorChange : function ( ) {
+            this.$el.css( 'color', this.rgbToHex( this.model.get( 'fontColor' ) ) );
         },
 
         onNameChange : function ( ) {
@@ -50,6 +64,15 @@ define( [
 
         onJobChange : function ( ) {
             this.$( '.job' ).text( this.model.get( 'job' ) );
+        },
+
+        rgbToHex : function ( rgb ) {
+            var rounded = {
+                r: rgb.r * 255,
+                g: rgb.g * 255,
+                b: rgb.b * 255
+            };
+            return '#' + ( 16777216 | rounded.b | ( rounded.g << 8 ) | ( rounded.r << 16 ) ).toString( 16 ).substr( 1 );
         },
 
     } );
@@ -73,6 +96,11 @@ define( [
         name  : 'radius'
     } );
 
+    appearance.createWidget( 'Background color', 'Color', {
+        model : card,
+        name : 'cardBackgroundColor'
+    } );
+
 
     // font appearance
     var fontAppearance = editor.createWidget( 'Group', {
@@ -83,6 +111,11 @@ define( [
         model : card,
         name  : 'fontsize'
     } );
+
+    fontAppearance.createWidget( 'Font color', 'Color', {
+        model : card,
+        name : 'fontColor'
+    })
 
 
     // text appearance
