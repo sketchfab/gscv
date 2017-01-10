725e92bc62a53340f0ec2cda973578c15011e78b
Nicolas Coden <nicolas@ncoden.fr>
2017-01-10 23:08:18 +0100

    ColorWidget: Fix hex to rgb conversion
    `r`, `g` and `b` values have a range of from 0 to 1, not 0 to 255.

--- sources/apis/editor/widgets/Color.js
+++ sources/apis/editor/widgets/Color.js
@@ -99,9 +99,9 @@ define( [
             // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
             var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
             return result ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
+                r: parseInt(result[1], 16) / 255,
+                g: parseInt(result[2], 16) / 255,
+                b: parseInt(result[3], 16) / 255
             } : null;
         },
 
