c515bba89140ee182c66d88e873848720cf6edab
Nicolas Coden <nicolas@ncoden.fr>
2017-01-10 09:41:24 +0100

    Add support for Title and Subtitle
    Use RichText widget to add support for Title and Subtitle customization
    on Card.

--- sources/app.js
+++ sources/app.js
@@ -14,6 +14,16 @@ define( [
         defaults : {
             radius : 10,
             padding: 50,
+            title: {
+                text: 'Name',
+                size: 30,
+                color: '#ffffff'
+            },
+            subtitle: {
+                text: 'Job',
+                size: 20,
+                color: '#bbbbbb'
+            }
         }
 
     } );
@@ -23,11 +33,27 @@ define( [
         initialize : function ( ) {
             this.model.on( 'change:radius', this.onRadiusChange, this );
             this.model.on( 'change:padding', this.onPaddingChange, this );
+            this.model.on( 'change:title', this.bindRichTextChange( '.name', 'title' ), this );
+            this.model.on( 'change:subtitle', this.bindRichTextChange( '.job', 'subtitle' ), this );
+            this.model.on( 'all', this.debugChange, this );
         },
 
         render : function ( ) {
             this.onRadiusChange( );
             this.onPaddingChange( );
+            this.bindRichTextChange( '.name', 'title' )();
+            this.bindRichTextChange( '.job', 'subtitle' )();
+        },
+
+        bindRichTextChange: function ( selector, key ) {
+            var $richText = this.$el.find( selector );
+
+            return function ( ) {
+                var datas = this.model.get( key );
+                $richText.text( datas.text );
+                $richText.css( 'font-size', datas.size );
+                $richText.css( 'color', datas.color );
+            }.bind( this );
         },
 
         onRadiusChange : function ( ) {
@@ -61,6 +87,22 @@ define( [
     appearance.createWidget( 'Paddings', 'NumberedSlider', {
         model : card,
         name  : 'padding'
+    } );
+
+    appearance.createWidget( 'Title', 'RichText', {
+        model : card,
+        name  : 'title',
+        colorOptions : {
+            format: 'hex'
+        },
+    } );
+
+    appearance.createWidget( 'Subtitle', 'RichText', {
+        model : card,
+        name  : 'subtitle',
+        colorOptions : {
+            format: 'hex'
+        },
     } );
 
 } );
