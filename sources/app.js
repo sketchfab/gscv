82b21dcf627a449dbd7161dba2cb0a6ebf01d031
kazes <florian.boudot@gmail.com>
2017-01-24 13:28:36 +0100

    simple text color change

--- sources/app.js
+++ sources/app.js
@@ -1,54 +1,70 @@
 /*global define*/
 
-define( [
+define([
 
     'vendors/Backbone',
     'vendors/JQuery',
 
     'editor'
 
-], function ( Backbone, $, editor ) {
+], function (Backbone, $, editor) {
 
-    var Card = Backbone.Model.extend( {
+    var Card = Backbone.Model.extend({
 
-        defaults : {
-            radius : 10
+        defaults: {
+            radius: 10,
+            color: "rgb(255,255,255)"
         }
 
-    } );
+    });
 
-    var View = Backbone.View.extend( {
+    var View = Backbone.View.extend({
 
-        initialize : function ( ) {
-            this.model.on( 'change:radius', this.onRadiusChange, this );
+        initialize: function () {
+            this.model.on('change:radius', this.onRadiusChange, this);
+            this.model.on('change:color', this.onColorChange, this);
         },
 
-        render : function ( ) {
-            this.onRadiusChange( );
+        render: function () {
+            this.onRadiusChange();
+            this.onColorChange();
         },
 
-        onRadiusChange : function ( ) {
-            this.$el.css( 'border-radius', this.model.get( 'radius' ) );
+        onRadiusChange: function () {
+            this.$el.css('border-radius', this.model.get('radius'));
+        },
+
+        onColorChange: function () {
+            var color = this.model.get('color');
+            var rgb = Object.values(color).map(function (color) {
+                return Math.floor(color * 255);
+            }).join(',');
+            this.$el.css('color', 'rgb(' + rgb + ')');
         }
 
-    } );
+    });
 
     // --- --- --- --- --- --- --- --- ---
 
-    var card = new Card( );
-    var view = new View( { model : card, el : $( '.card' ) } );
+    var card = new Card();
+    var view = new View({model: card, el: $('.card')});
 
-    view.render( );
+    view.render();
 
     // --- --- --- --- --- --- --- --- ---
 
-    var appearance = editor.createWidget( 'Group', {
-        label : 'Card Appearance'
-    } );
+    var appearance = editor.createWidget('Group', {
+        label: 'Card Appearance'
+    });
 
-    appearance.createWidget( 'Border radius', 'NumberedSlider', {
-        model : card,
-        name  : 'radius'
-    } );
+    appearance.createWidget('Border radius', 'NumberedSlider', {
+        model: card,
+        name: 'radius'
+    });
 
-} );
+    appearance.createWidget('Text color', 'Color', {
+        model: card,
+        name: 'color'
+    });
+
+});
