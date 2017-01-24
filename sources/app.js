c9a58363c4dc975859a0220dcf478fe0259f3a81
kazes <florian.boudot@gmail.com>
2017-01-24 23:02:09 +0100

    name and job are text editable + localStorage stores name, job, and radius (color not working yet)

--- sources/app.js
+++ sources/app.js
@@ -10,17 +10,22 @@ define([
 ], function (Backbone, $, editor) {
 
     var Card = Backbone.Model.extend({
-
         defaults: {
-            radius: 10,
-            color: "rgb(255,255,255)"
+            radius: localStorage.getItem('radius') * 1 || 10,
+            color: JSON.parse(localStorage.getItem('color')) // sorry, default color is not working :(
         }
-
     });
 
     var View = Backbone.View.extend({
 
         initialize: function () {
+            this.$el.find('[contenteditable=true]').on('input', this.onTextChange);
+            this.$el.find('[contenteditable=true]').each(function () {
+                var def_text = localStorage.getItem(this.getAttribute('class'));
+                if (def_text) {
+                    this.innerHTML = def_text;
+                }
+            });
             this.model.on('change:radius', this.onRadiusChange, this);
             this.model.on('change:color', this.onColorChange, this);
         },
@@ -31,11 +36,18 @@ define([
         },
 
         onRadiusChange: function () {
-            this.$el.css('border-radius', this.model.get('radius'));
+            var radius = this.model.get('radius');
+            this.$el.css('border-radius', radius);
+            localStorage.setItem('radius', radius)
+        },
+
+        onTextChange: function () {
+            localStorage.setItem(this.getAttribute('class'), this.innerHTML);
         },
 
         onColorChange: function () {
             var color = this.model.get('color');
+            localStorage.setItem('color', JSON.stringify(color));
             var rgb = Object.values(color).map(function (color) {
                 return Math.floor(color * 255);
             }).join(',');
